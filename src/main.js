4d77b588305d87c23ea656306b791d595f804f81
Farah Hasna Pangesti <farah.pangesti@gmail.com>
2025-06-04 14:31:42 +0700

    init repo

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Typed from "typed.js";
+
+const myTyped = new Typed("#greeting", {
+    strings: ["Data Analyst", "Mathematics Teacher", "Business Owner"],
+    loop: true,
+    typeSpeed: 70
+});
